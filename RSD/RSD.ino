9b603e9ff8d54f0dccdf0f7d1e59dc6ed05761b5
60015031 <56292779+Pongsagorn1@users.noreply.github.com>
2020-02-25 10:10:13 +0700

    Add files via upload

---
+++ RSD/RSD.ino
@@ -0,0 +1,69 @@
+#include <LiquidCrystal_PCF8574.h>
+#include <DS1307RTC.h>
+#include <Time.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+int num = 0;
+LiquidCrystal_PCF8574 lcd(0x3F);
+File myFile;
+const int chipSelect = 10;
+
+void setup() {
+  lcd.setBacklight(255);
+  lcd.begin(20,4);
+  lcd.display();
+  Serial.begin(9600);
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD Card initialization failed!");
+    return;  
+  }
+  ReadText();
+}
+
+void loop() {
+  
+}
+
+
+void ReadText(){
+  myFile = SD.open("test.txt",FILE_READ);
+  if (myFile) {
+      while (myFile.available()) {  
+        char readByte = myFile.read();
+          Serial.write(readByte);
+          //lcd.write(readByte);
+        if (readByte != '\n'){
+          //lcd.print(readByte);
+          lcd.write(readByte);
+          delay(100);
+        }else if(num - 3 == 0){
+          lcd.setCursor(0,0);
+          num = 0;
+          delay(1000);
+          lcd.clear();
+          delay(1000);
+        }else{
+          num++;
+          lcd.setCursor(0,num);
+        }
+
+}
+    // close the file:
+    myFile.close();
+  } 
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ W2SD/W2SD.ino
@@ -0,0 +1,121 @@
+#include <DS1307RTC.h>
+#include <Time.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+const int chipSelect = 10;
+int num = 0;
+String SID = "60015031";
+String time ;
+tmElements_t tm;
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) ; // wait for serial
+  delay(200);
+  Serial.println("ArduinoAll DataLogger Shield Test");
+  pinMode(SS, OUTPUT);
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD Card initialization failed!");
+    return;  
+  }
+  Serial.println("SD Card OK.");
+  ReadText();
+}
+
+void loop() {
+  if (num == 0){
+    time = Now();
+    WriteText(SID+" in");
+    Serial.println(time);
+    WriteText(time);
+    num = 1;
+  }else if (num == 1){
+    time = Now();
+    WriteText(SID+" out");
+    Serial.println(time);
+    WriteText(time);
+    num = 0;
+  }
+  delay(3000);
+}
+
+
+void ReadText(){
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } 
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void WriteText(String txt){
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.println(txt);
+    myFile.close();
+  } 
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+
+String Now(){
+  String time = "";
+  if (RTC.read(tm)) {
+    //    time = String(tm.Hour+":"+tm.Minute+":"+tm.Secnd+" DAY : "+tm.Day+"/"+tm.Month+"/"+tmYearToCalendar(tm.Year));
+    time+=tm.Hour;
+    time+=":";
+
+    time+=tm.Minute;
+    time+=":";
+
+    time+=tm.Second;
+    time+=" ";
+
+    time+=tm.Day;
+    time+="/";
+
+    time+=tm.Month;
+    time+="/";
+
+    time+=tmYearToCalendar(tm.Year);
+  } 
+  else {
+    time = "NO";
+    if (RTC.chipPresent()) {
+      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+      Serial.println("example to initialize the time and begin running.");
+      Serial.println();
+    } 
+    else {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+      Serial.println();
+    }
+  }
+  return time;
+}
+
+
+
+
+
+
+
